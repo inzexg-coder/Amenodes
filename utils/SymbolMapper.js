9509facb0e97547e12a67211035ba209ec5b97a2
Amenoke <inzexg@gmail.com>
2026-04-30 13:17:25 +0300

    Create SymbolMapper.js

---
+++ utils/SymbolMapper.js
@@ -0,0 +1,22 @@
+export const SYMBOL_MAP = {
+  $alpha:'α', $beta:'β', $gamma:'γ', $delta:'δ', $epsilon:'ε',
+  $zeta:'ζ', $eta:'η', $theta:'θ', $iota:'ι', $kappa:'κ',
+  $lambda:'λ', $mu:'μ', $nu:'ν', $xi:'ξ', $omicron:'ο',
+  $pi:'π', $rho:'ρ', $sigma:'σ', $tau:'τ', $upsilon:'υ',
+  $phi:'φ', $chi:'χ', $psi:'ψ', $omega:'ω',
+  $Alpha:'Α', $Beta:'Β', $Gamma:'Γ', $Delta:'Δ', $Epsilon:'Ε',
+  $Zeta:'Ζ', $Eta:'Η', $Theta:'Θ', $Iota:'Ι', $Kappa:'Κ',
+  $Lambda:'Λ', $Mu:'Μ', $Nu:'Ν', $Xi:'Ξ', $Omicron:'Ο',
+  $Pi:'Π', $Rho:'Ρ', $Sigma:'Σ', $Tau:'Τ', $Upsilon:'Υ',
+  $Phi:'Φ', $Chi:'Χ', $Psi:'Ψ', $Omega:'Ω',
+  $e:'e', $inf:'∞', $sqrt:'√', $sum:'∑', $int:'∫',
+  $partial:'∂', $nabla:'∇', $in:'∈', $notin:'∉', $subset:'⊂',
+  $supset:'⊃', $union:'∪', $intersect:'∩', $emptyset:'∅',
+  $degree:'°', $plusmn:'±', $times:'×', $div:'÷',
+  $leq:'≤', $geq:'≥', $neq:'≠', $approx:'≈'
+};
+
+export function replaceSymbols(s) {
+  if (!s) return s;
+  return s.replace(/\$[a-zA-Z]+/g, m => SYMBOL_MAP[m] || m);
+}
