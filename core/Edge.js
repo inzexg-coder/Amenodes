993cd318d18d7ec0c76d2873bd55da10ecab5a89
Amenoke <inzexg@gmail.com>
2026-04-30 13:18:40 +0300

    Create Edge.js

---
+++ core/Edge.js
@@ -0,0 +1,17 @@
+export class Edge {
+  constructor(id, sourceId, targetId, port = 'main') {
+    this.id = id;
+    this.sourceId = sourceId;
+    this.targetId = targetId;
+    this.sourcePort = port;
+  }
+
+  toJSON() {
+    return {
+      id: this.id,
+      sourceId: this.sourceId,
+      targetId: this.targetId,
+      sourcePort: this.sourcePort
+    };
+  }
+}
