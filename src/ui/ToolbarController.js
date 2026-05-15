09b811e24b656f19f575fc410f91efbdd5ac5499
Amenoke <inzexg@gmail.com>
2026-05-15 20:18:18 +0300

    Update ToolbarController.js

--- src/ui/ToolbarController.js
+++ src/ui/ToolbarController.js
@@ -18,36 +18,35 @@ export class ToolbarController {
 
     document.getElementById('undoBtn').onclick = () => this.undo();
     document.getElementById('redoBtn').onclick = () => this.redo();
-    document.getElementById('addEmptyBtn').onclick = () => this.addNumberNode();
-    document.getElementById('addConstantBtn').onclick = () => this.addConstantNode();
-    document.getElementById('addGroupBtn').onclick = () => this.addGroupNode();
-    document.getElementById('addOutputBtn').onclick = () => this.addOutputNode();
     document.getElementById('exportBtn').onclick = () => this.export();
     document.getElementById('importBtn').onclick = () => this.import();
     document.getElementById('clearStorageBtn').onclick = () => this.clearStorage();
     document.getElementById('fileInput').onchange = (e) => this.handleFileImport(e);
     
-    const fileGroup = document.querySelector('.file-group');
-    if (fileGroup) {
-      const langContainer = document.createElement('div');
-      langContainer.id = 'languageSwitcherContainer';
-      langContainer.style.display = 'inline-block';
-      langContainer.style.marginLeft = '8px';
-      fileGroup.parentNode.insertBefore(langContainer, fileGroup.nextSibling);
-      this.languageSwitcher = new LanguageSwitcher('languageSwitcherContainer');
-    }
+    this.initLanguageSwitcher();
 
     i18n.subscribe(() => this.updateButtonTexts());
+  }
+  
+  initLanguageSwitcher() {
+    let langContainer = document.getElementById('languageSwitcherContainer');
+    if (!langContainer) {
+      langContainer = document.createElement('div');
+      langContainer.id = 'languageSwitcherContainer';
+      langContainer.style.display = 'inline-block';
+
+      const fileGroup = document.querySelector('.file-group');
+      if (fileGroup && fileGroup.parentNode) {
+        fileGroup.parentNode.appendChild(langContainer);
+      }
+    }
+    this.languageSwitcher = new LanguageSwitcher('languageSwitcherContainer');
   }
   
   updateButtonTexts() {
     const buttons = {
       undoBtn: t('toolbar.undo'),
       redoBtn: t('toolbar.redo'),
-      addEmptyBtn: t('toolbar.number'),
-      addConstantBtn: t('toolbar.constant'),
-      addGroupBtn: t('toolbar.group'),
-      addOutputBtn: t('toolbar.output'),
       exportBtn: t('toolbar.export'),
       importBtn: t('toolbar.import'),
       clearStorageBtn: t('toolbar.clearStorage')
@@ -79,47 +78,6 @@ export class ToolbarController {
       x: (rect.width / 2 - off.x) / zoom,
       y: (rect.height / 2 - off.y) / zoom
     };
-  }
-
-  addNumberNode() {
-    const { x, y } = this.getCenterCoords();
-    const node = NodeFactory.createNumberAt(x - 100, y - 30);
-    this.graph.addNode(node);
-    this.renderer.render();
-    this.history.save();
-  }
-  
-  async addConstantNode() {
-    const { x, y } = this.getCenterCoords();
-    const input = await modal.prompt(t('modal.enterValue'), '0');
-    
-    if (input !== null && input !== undefined && input !== '') {
-      const value = parseFloat(input);
-      const finalValue = isNaN(value) ? 0 : value;
-      
-      const node = NodeFactory.createConstantAt(x - 100, y - 30, finalValue);
-      this.graph.addNode(node);
-      this.graph.reevaluateAll();
-      this.graph.updateAllOutputs();
-      this.renderer.render();
-      this.history.save();
-    }
-  }
-  
-  addGroupNode() {
-    const { x, y } = this.getCenterCoords();
-    const node = NodeFactory.createGroupAt(x - 120, y - 30);
-    this.graph.addNode(node);
-    this.renderer.render();
-    this.history.save();
-  }
-
-  addOutputNode() {
-    const { x, y } = this.getCenterCoords();
-    const node = NodeFactory.createOutputAt(x - 100, y - 30);
-    this.graph.addNode(node);
-    this.renderer.render();
-    this.history.save();
   }
 
   export() {
