6bb8dfe23a305cdd1ae1fd4df82f6a9976b11146
Amenoke <inzexg@gmail.com>
2026-05-15 20:05:42 +0300

    Update NodeFactory.js

--- src/nodes/NodeFactory.js
+++ src/nodes/NodeFactory.js
@@ -1,14 +1,44 @@
-import { NumberNode } from './NumberNode.js';
-import { GroupNode } from './GroupNode.js';
-import { CalcNode } from './CalcNode.js';
-import { OutputNode } from './OutputNode.js';
-import { MapNode } from './MapNode.js';
-import { ConstantNode } from './ConstantNode.js';
-import { ConfidenceIntervalNode } from './ConfidenceIntervalNode.js';
+import { NumberNode, metadata as numberMeta } from './NumberNode.js';
+import { ConstantNode, metadata as constantMeta } from './ConstantNode.js';
+import { GroupNode, metadata as groupMeta } from './GroupNode.js';
+import { CalcNode, metadata as calcMeta } from './CalcNode.js';
+import { OutputNode, metadata as outputMeta } from './OutputNode.js';
+import { MapNode, metadata as mapMeta } from './MapNode.js';
+import { ConfidenceIntervalNode, metadata as confidenceMeta } from './ConfidenceIntervalNode.js';
 import { i18n } from '../i18n/LanguageManager.js';
 
+const nodeRegistry = new Map();
+const nodeTypesList = [];
+
+function registerNode(type, ctor, metadata) {
+  nodeRegistry.set(type, { ctor, metadata });
+  nodeTypesList.push({ type, ...metadata });
+}
+
+registerNode('number', NumberNode, numberMeta);
+registerNode('constant', ConstantNode, constantMeta);
+registerNode('group', GroupNode, groupMeta);
+registerNode('calc', CalcNode, calcMeta);
+registerNode('output', OutputNode, outputMeta);
+registerNode('map', MapNode, mapMeta);
+registerNode('confidenceInterval', ConfidenceIntervalNode, confidenceMeta);
+
 export class NodeFactory {
+  static getAvailableNodeTypes() {
+    return nodeTypesList;
+  }
+
+  static getNodeMetadata(type) {
+    return nodeRegistry.get(type)?.metadata || null;
+  }
+
   static createNode(type, options = {}) {
+    const registered = nodeRegistry.get(type);
+    if (!registered) {
+      throw new Error(`Unknown node type: ${type}`);
+    }
+    
+    const { ctor } = registered;
     const { id, x, y, title, ...rest } = options;
     
     const defaultTitle = i18n.t(`nodes.${type}`);
@@ -16,21 +46,21 @@ export class NodeFactory {
     
     switch(type) {
       case 'number':
-        return new NumberNode(id || 0, x || 0, y || 0, finalTitle, rest.val ?? 0);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.val ?? 0);
       case 'constant':
-        return new ConstantNode(id || 0, x || 0, y || 0, finalTitle, rest.val ?? 0);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.val ?? 0);
       case 'group':
-        return new GroupNode(id || 0, x || 0, y || 0, finalTitle, rest.vals || [{ name: `${i18n.t('common.value')} 1`, val: 0 }]);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.vals || [{ name: `${i18n.t('common.value')} 1`, val: 0 }]);
       case 'calc':
-        return new CalcNode(id || 0, x || 0, y || 0, finalTitle, rest.calcType || 'div3');
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.calcType || 'div3');
       case 'output':
-        return new OutputNode(id || 0, x || 0, y || 0, finalTitle, rest.rows || []);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.rows || []);
       case 'map':
-        return new MapNode(id || 0, x || 0, y || 0, finalTitle, rest.maps || [{ x: 0, y: 0 }]);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest.maps || [{ x: 0, y: 0 }]);
       case 'confidenceInterval':
-        return new ConfidenceIntervalNode(id || 0, x || 0, y || 0, finalTitle);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle);
       default:
-        throw new Error(`Unknown node type: ${type}`);
+        return new ctor(id || 0, x || 0, y || 0, finalTitle, rest);
     }
   }
 
