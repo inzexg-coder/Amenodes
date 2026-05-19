64c2dd3f62715c440c7354987eac7b62a14878f2
Amenoke <inzexg@gmail.com>
2026-05-20 00:08:29 +0300

    Update confidenceInterval.js

--- src/nodes/locales/en/confidenceInterval.js
+++ src/nodes/locales/en/confidenceInterval.js
@@ -4,5 +4,9 @@ export default {
   confidence: {
     uncertaintyInputs: 'Uncertainty inputs',
     multiplierInputs: 'Multiplier inputs'
+  },
+  dataTypes: {
+    interval: 'Interval',
+    uncert: 'Uncertainty'
   }
 };
