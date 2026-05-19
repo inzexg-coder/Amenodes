f4874a300fbc46d1e474878109a1d397e7261845
Amenoke <inzexg@gmail.com>
2026-05-20 00:08:55 +0300

    Update group.js

--- src/nodes/locales/ru/group.js
+++ src/nodes/locales/ru/group.js
@@ -1,5 +1,8 @@
 export default {
   nodes: { group: 'Группа чисел' },
   nodeDescriptions: { group: 'Набор именованных числовых значений' },
-  group: { addValue: '+ Добавить значение' }
+  group: { addValue: '+ Добавить значение' },
+  dataTypes: {
+    array: 'Массив'
+  }
 };
