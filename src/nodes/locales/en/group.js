c3ce74c3d450cd6bed3de9e74cdfdd1639b00a1e
Amenoke <inzexg@gmail.com>
2026-05-20 00:07:23 +0300

    Update group.js

--- src/nodes/locales/en/group.js
+++ src/nodes/locales/en/group.js
@@ -1,5 +1,8 @@
 export default {
   nodes: { group: 'Number Group' },
   nodeDescriptions: { group: 'A collection of named numeric values' },
-  group: { addValue: '+ Add value' }
+  group: { addValue: '+ Add value' },
+  dataTypes: {
+    array: 'Array'
+  }
 };
