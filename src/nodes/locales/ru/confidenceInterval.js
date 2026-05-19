bba7216d5ccdc8a3cf544ec006555bfdb0e52898
Amenoke <inzexg@gmail.com>
2026-05-19 18:03:12 +0300

    Add Russian localization for confidenceInterval node

---
+++ src/nodes/locales/ru/confidenceInterval.js
@@ -0,0 +1,8 @@
+export default {
+  nodes: { confidenceInterval: 'Доверительный интервал' },
+  nodeDescriptions: { confidenceInterval: 'Вычисляет доверительный интервал, умножая погрешность на множитель' },
+  confidence: {
+    uncertaintyInputs: 'Входов с погрешностью',
+    multiplierInputs: 'Входов-множителей'
+  }
+};
