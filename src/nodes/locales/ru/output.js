9957e0b3b50c81a4a54198fa1d1b5d025011c1dc
Amenoke <inzexg@gmail.com>
2026-05-19 18:02:44 +0300

    Create output.js

---
+++ src/nodes/locales/ru/output.js
@@ -0,0 +1,9 @@
+export default {
+  nodes: { output: 'Вывод' },
+  nodeDescriptions: { output: 'Отображает результаты подключённых узлов в виде таблицы' },
+  output: {
+    title: 'Вывод результатов',
+    noConnections: 'Нет связей',
+    valueCount: 'знач.'
+  }
+};
