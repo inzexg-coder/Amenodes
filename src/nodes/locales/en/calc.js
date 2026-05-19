781e8c7f249b07865485d43b1dc33f8e7f60ffaf
Amenoke <inzexg@gmail.com>
2026-05-19 18:00:14 +0300

    Add localization for uncertainty calculation node

---
+++ src/nodes/locales/en/calc.js
@@ -0,0 +1,11 @@
+export default {
+  nodes: { calc: 'Uncertainty' },
+  nodeDescriptions: { calc: 'Uncertainty calculation node' },
+  calcTypes: {
+    div3: 'Measurement Uncertainty',
+    div_sqrt12: 'Rounding Uncertainty',
+    sqrt_sum_sq: 'Combined Uncertainty',
+    result: 'Result',
+    inputs: 'inputs'
+  }
+};
