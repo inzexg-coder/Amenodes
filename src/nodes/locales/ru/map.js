dede342b377c117619f695dd0de6389515050973
Amenoke <inzexg@gmail.com>
2026-05-19 18:02:55 +0300

    Create map.js

---
+++ src/nodes/locales/ru/map.js
@@ -0,0 +1,12 @@
+export default {
+  nodes: { map: 'Карта' },
+  nodeDescriptions: { map: 'Преобразует входные значения в выходные по правилам X→Y' },
+  map: {
+    title: 'Карта преобразований',
+    xCol: 'x',
+    yCol: 'y',
+    addRule: '+ Добавить правило',
+    passThrough: 'Сквозной проход',
+    separateOutput: 'Отдельный голубой выход'
+  }
+};
