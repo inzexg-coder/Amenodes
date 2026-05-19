7110537136cfded7633ef82e05d394204be8a125
Amenoke <inzexg@gmail.com>
2026-05-19 17:09:35 +0300

    Refactor index.js to export node modules directly

--- src/nodes/index.js
+++ src/nodes/index.js
@@ -1,17 +1,7 @@
-import { NumberNode, metadata as numberMeta } from './NumberNode.js';
-import { ConstantNode, metadata as constantMeta } from './ConstantNode.js';
-import { GroupNode, metadata as groupMeta } from './GroupNode.js';
-import { CalcNode, metadata as calcMeta } from './CalcNode.js';
-import { OutputNode, metadata as outputMeta } from './OutputNode.js';
-import { MapNode, metadata as mapMeta } from './MapNode.js';
-import { ConfidenceIntervalNode, metadata as confidenceMeta } from './ConfidenceIntervalNode.js';
-
-export const NODES = [
-  { type: 'number', NodeClass: NumberNode, metadata: numberMeta },
-  { type: 'constant', NodeClass: ConstantNode, metadata: constantMeta },
-  { type: 'group', NodeClass: GroupNode, metadata: groupMeta },
-  { type: 'calc', NodeClass: CalcNode, metadata: calcMeta },
-  { type: 'output', NodeClass: OutputNode, metadata: outputMeta },
-  { type: 'map', NodeClass: MapNode, metadata: mapMeta },
-  { type: 'confidenceInterval', NodeClass: ConfidenceIntervalNode, metadata: confidenceMeta },
-];
+export * from './NumberNode.js';
+export * from './ConstantNode.js';
+export * from './GroupNode.js';
+export * from './CalcNode.js';
+export * from './OutputNode.js';
+export * from './MapNode.js';
+export * from './ConfidenceIntervalNode.js';
