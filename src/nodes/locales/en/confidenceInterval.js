c6bf563f7612cf6a1f6ddfd46aa086bc7c4a1dd3
Amenoke <inzexg@gmail.com>
2026-05-19 18:01:15 +0300

    Add confidenceInterval localization file

---
+++ src/nodes/locales/en/confidenceInterval.js
@@ -0,0 +1,8 @@
+export default {
+  nodes: { confidenceInterval: 'Confidence Interval' },
+  nodeDescriptions: { confidenceInterval: 'Calculates confidence interval by multiplying uncertainty with a multiplier' },
+  confidence: {
+    uncertaintyInputs: 'Uncertainty inputs',
+    multiplierInputs: 'Multiplier inputs'
+  }
+};
