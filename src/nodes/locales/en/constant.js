a40994b06ef9e9f3d412a8acf56046ff4e7210dc
Amenoke <inzexg@gmail.com>
2026-05-20 00:04:12 +0300

    Update constant.js

--- src/nodes/locales/en/constant.js
+++ src/nodes/locales/en/constant.js
@@ -1,4 +1,7 @@
 export default {
   nodes: { constant: 'Constant' },
-  nodeDescriptions: { constant: 'A constant value that never changes' }
+  nodeDescriptions: { constant: 'A constant value that never changes' },
+  dataTypes: {
+    num: 'Number'
+  }
 };
