6bc17e3070a758ebf36e5e07defd5ff2c22e8546
Amenoke <inzexg@gmail.com>
2026-06-01 18:20:08 +0300

    Sem node (#40)
    * Add SEMNode class for statistical error measurement
    
    * Create sem.js
    
    * Create sem.js
    
    * Update this-manifest.js
    
    * Update SEMNode.js
    
    * Update SEMNode.js
    
    * Update NODES.md

---
+++ src/nodes/locales/en/sem.js
@@ -0,0 +1,13 @@
+export default {
+  nodes: {
+    sem: 'Standard Error of Mean'
+  },
+  nodeDescriptions: {
+    sem: 'Computes the standard error of the mean from an array of numbers.'
+  },
+  sem: {
+    inputs: 'Input connections',
+    sampleSize: 'Sample size',
+    result: 'SEM'
+  }
+};
---
+++ src/nodes/locales/ru/sem.js
@@ -0,0 +1,13 @@
+export default {
+  nodes: {
+    sem: 'Стандартная ошибка среднего'
+  },
+  nodeDescriptions: {
+    sem: 'Вычисляет стандартную ошибку среднего из массива чисел. '
+  },
+  sem: {
+    inputs: 'Входных связей',
+    sampleSize: 'Объём выборки',
+    result: 'SEM'
+  }
+};
