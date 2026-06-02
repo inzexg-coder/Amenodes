7447be117399f8155fb65161a4a24bcef67ed3ba
Amenoke <inzexg@gmail.com>
2026-06-02 18:41:54 +0300

    Update this-manifest.js

--- src/nodes/manifest/this-manifest.js
+++ src/nodes/manifest/this-manifest.js
@@ -8,7 +8,6 @@ import { GroupNode, metadata as groupMeta } from '../GroupNode.js';
 import { CalcNode, metadata as calcMeta } from '../CalcNode.js';
 import { OutputNode, metadata as outputMeta } from '../OutputNode.js';
 import { MapNode, metadata as mapMeta } from '../MapNode.js';
-import { ConfidenceIntervalNode, metadata as confidenceMeta } from '../ConfidenceIntervalNode.js';
 import { MeanNode, metadata as meanMeta } from '../MeanNode.js';
 import { SEMNode, metadata as semMeta } from '../SEMNode.js';
 
@@ -25,7 +24,6 @@ export const nodesManifest = [
   { ctor: CalcNode, metadata: calcMeta, fileName: 'calc.js' },
   { ctor: OutputNode, metadata: outputMeta, fileName: 'output.js' },
   { ctor: MapNode, metadata: mapMeta, fileName: 'map.js' },
-  { ctor: ConfidenceIntervalNode, metadata: confidenceMeta, fileName: 'confidenceInterval.js' },
   { ctor: MeanNode, metadata: meanMeta, fileName: 'mean.js' },
   { ctor: SEMNode, metadata: semMeta, fileName: 'sem.js' },
 
